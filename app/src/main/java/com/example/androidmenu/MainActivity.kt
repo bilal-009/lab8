fef5afed6bc7da23479caa4b2aacb7850190f1b4
adeemakhtar <adeemakhtar@gmail.com>
2024-11-28 23:05:50 +0500

    Android Scrollable

---
+++ app/src/main/java/com/example/androidmenu/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.androidmenu
+
+import android.os.Bundle
+import android.view.RoundedCorner
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androidmenu.ui.theme.AndroidMenuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidMenuTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    hori_list_buttons()
+                    ver_list_buttons()
+                        //MenuGrid()
+                    }
+            }
+        }
+    }
+}
+
+@Composable
+fun hori_list_buttons(){
+    Row (modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Catagories.forEach {
+            MenuCatagory(catagory = it)
+        }
+    }
+}
+
+@Composable
+fun ver_list_buttons(){
+    Column (modifier=Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.Center) {
+        Catagories.forEach {
+            MenuCatagory(catagory = it)
+        }
+    }
+}
+
+@Composable
+fun MenuCatagory(catagory: String) {
+    Button(
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(Color.LightGray),
+        shape = RoundedCornerShape(40),
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Text(text = catagory)
+    }
+}
+
+val Catagories = listOf<String>(
+    "Lunch",
+    "Dessert",
+    "Mains"
+)
+
+
+@Preview(showBackground = true)
+@Composable
+fun MenuCatagoryPreview() {
+    AndroidMenuTheme {
+        hori_list_buttons()
+        ver_list_buttons()
+    }
+}
---
+++ app/src/main/java/com/example/androidmenu/MenuGrid.kt
@@ -0,0 +1,87 @@
+package com.example.androidmenu
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun MenuGrid(){
+    Column(modifier=Modifier.verticalScroll(rememberScrollState())) {
+        repeat(10){
+            Row (modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    repeat(2){
+                        GridCell()
+                    }
+            }
+        }
+    }
+}
+
+@Composable
+fun GridCell(){
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 8.dp), modifier = Modifier.padding(8.dp)) {
+        Box(modifier = Modifier
+            .requiredSize(180.dp, 180.dp)
+            .padding(8.dp))
+        {
+            Image(
+                painter = painterResource(id = R.drawable.greeksalad),
+                contentDescription = "Greek Salad",
+                Modifier
+                    .height(180.dp)
+                    .width(180.dp)
+            )
+            Text(
+                text = "Greek Salad",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .background(Color.White)
+                    .align(Alignment.TopStart)
+            )
+            Text(
+                text = "$12.5",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(start = 4.dp, end = 4.dp)
+                    .align(Alignment.BottomEnd)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GridCellPreview(){
+    MenuGrid()
+}
